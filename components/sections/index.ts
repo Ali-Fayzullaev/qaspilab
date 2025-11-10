6ae3c746ed966b174c3d73ceb2197f8a44c22775
Ali-Fayzullaev <alifayzullaev2020@gmail.com>
2025-11-10 15:50:26 +0500

    fix: Update AboutSection import to use AboutSectionOptimized for consistency

--- components/sections/index.ts
+++ components/sections/index.ts
@@ -1,7 +1,7 @@
 export { default as HeroSection } from './HeroSection';
 export { default as MissionSection } from './MissionSection';
 export { default as MissionSectionOptimized } from './MissionSectionOptimized';
-export { default as AboutSection } from './AboutSection';
+export { default as AboutSection } from './AboutSectionOptimized';
 export { default as CTASection } from './CTASection';
 export { default as GlobalReachSection } from './GlobalReachSectionPremium';
 export { default as WhyUsSection } from './WhyUsSectionPremium';
